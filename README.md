fd80f26caf642dd378420cc8d1b308d9ef0c6c6d
Frederik Berg <frederik.berg@sevenval.com>
2016-10-12 17:46:25 +0200

    general overhaul and improvements

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # batmodals
+
+Tested Browserscope:
+
+- IE 11+ (could be adjusted for IE10)
+- Edge 11+
+- Chrome ?+
+- Firefox ?+
+
+Required features:
+- ecmascript 5+
+- flexbox
+- dataset
+
+
+Usage:
+every modal needs to look like this:
+```
+<div class="modal" data-modal id="m5" hidden>
+  <div class="modal__content" style="width:38vw">
+    [any content here]
+  </div>
+</div>
+```
+there needs to be a node  after the last visible node in the document body, which needs to have the following properties:
+```
+<div class="modal__helper" data-modal-helper tabindex="0"></div>
+```
+modals can be defined anywhere, they will be dynamically moved to right in front of the tabHelper
---
+++ script.js
@@ -0,0 +1,142 @@
+'use strict';
+(function (modal) {
+  var debugmode = 0;
+  var argv = (decodeURI(Array.prototype.slice.call(document.getElementsByTagName('script')).pop().src).split('?', 2)[1] || '').split('&');
+  if (argv.indexOf('debug') !== -1) {
+    debugmode = 1;
+  }
+  if (typeof modal !== 'undefined') {
+    // already initialised
+    if (debugmode) {
+      console.log('modals.js already loaded in version: ' + modal.version);
+    }
+    return;
+  }
+
+  // The namespace object.
+  modal = {};
+
+  // Namespaced 'global' variables.
+
+  // Public methods, exposed to namespace.
+
+  modal.show = function() {
+    if (!document.querySelectorAll('[data-modal]:not([hidden])').length) {
+      document.body.style.paddingRight = window.innerWidth - document.body.clientWidth + 'px';
+      document.body.classList.add('noscroll');
+    }
+    // document.body.appendChild(this);
+    document.body.insertBefore(this, document.querySelector('[data-modal-helper]') );
+    this.removeAttribute('hidden');
+    this.focusBefore = forceFocus(this);
+  };
+
+  modal.close = function() {
+    if (this === window || !this.dataset.hasOwnProperty('modal')) {
+      return;
+    }
+    this.setAttribute('hidden', '');
+    if (this.focusBefore) {
+      forceFocus(this.focusBefore);
+      delete this.focusBefore;
+    }
+    if (document.querySelectorAll('[data-modal]:not([hidden])').length) {
+      document.body.insertBefore(this, document.querySelector('[data-modal]:not([hidden])'));
+    } else {
+      document.body.style.paddingRight = '';
+      document.body.classList.remove('noscroll');
+    }
+  };
+
+  modal.initShowButton = function(node) {
+    node.addEventListener('click', buttonListener);
+  };
+
+  modal.initAllShowButtons = function() {
+    Array.prototype.slice.call(document.querySelectorAll('[data-modal-show]')).forEach(modal.initShowButton);
+  };
+
+  modal.initModal = function(node) {
+    node.addEventListener('click', modalListener);
+  };
+
+  modal.initAllModals = function() {
+    Array.prototype.slice.call(document.querySelectorAll('[data-modal]')).forEach(modal.initModal);
+  };
+
+  modal.initTabtrap = function() {
+    document.body.addEventListener('keyup', tabtrapListener);
+  };
+
+  modal.initAll = function() {
+    modal.initAllShowButtons();
+    // modal.initAllHideButtons();
+    modal.initAllModals();
+    modal.initTabtrap();
+  };
+
+  // Internal methods, not exposed to namespace.
+
+  function forceFocus(node) {
+    var ret = document.activeElement;
+    if (typeof node === 'string') {
+      node = document.getElementById(node) || document.querySelector(node);
+    }
+    if (node.tabIndex === -1) {
+      if (node.hasAttribute('tabindex')) {
+        // node.setAttribute('tabindex', 0);
+        node.focus();
+        // node.setAttribute('tabindex', -1);
+      } else {
+        node.setAttribute('tabindex', 0);
+        node.focus();
+        node.removeAttribute('tabindex');
+      }
+    } else {
+      node.focus();
+    }
+    return ret;
+  }
+
+  // @this
+  function buttonListener() {
+    modal.show.call(document.getElementById(this.dataset.modalShow));
+  }
+
+  // @this
+  function modalListener(event) {
+    if (this === event.target) {
+      modal.close.call(this);
+    }
+  }
+
+  function tabtrapListener(event) {
+    var activeModal = Array.prototype.slice.call(document.querySelectorAll('[data-modal]:not([hidden])')).pop();
+    if (!activeModal) {
+      return;
+    }
+    if (event.keyCode === 27) { // 27 == esc
+      modal.close.call(activeModal);
+      return;
+    }
+    if (event.keyCode === 9) { // 9 == tab
+      if (activeModal.contains(document.activeElement)) {
+        return;
+      }
+      var focusables = Array.prototype.slice.call(activeModal.querySelectorAll('button, input, [tabindex]:not([tabindex="-1"])'));
+      if (focusables.length === 0) {
+        modal.focus();
+        return;
+      }
+      // focus last/first element depending on forward/backward tabbing
+      focusables[document.activeElement.dataset.hasOwnProperty('modalHelper')?0:focusables.length-1].focus();
+      return;
+    }
+  }
+
+
+  if (argv.indexOf('init') !== -1) {
+    // auto initialize if script is loaded with a src of 'modals.js?init'
+    modal.initAll();
+  }
+}(window.modal));
